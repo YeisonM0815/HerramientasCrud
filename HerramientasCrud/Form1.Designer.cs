54e9966ec8a44219eba7e3c00fa036fad92d4336
marinyeison452@gmail.com <marinyeison452@gmail.com>
2024-09-04 21:15:19 -0500

    From y conexion lista

--- HerramientasCrud/Form1.Designer.cs
+++ HerramientasCrud/Form1.Designer.cs
@@ -28,12 +28,305 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label2 = new Label();
+            textBox3 = new TextBox();
+            label3 = new Label();
+            textBox4 = new TextBox();
+            label4 = new Label();
+            textBox5 = new TextBox();
+            label5 = new Label();
+            textBox6 = new TextBox();
+            numericUpDown1 = new NumericUpDown();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            label6 = new Label();
+            label7 = new Label();
+            button4 = new Button();
+            button5 = new Button();
+            dataGridView1 = new DataGridView();
+            Column1 = new DataGridViewTextBoxColumn();
+            Column2 = new DataGridViewTextBoxColumn();
+            Column3 = new DataGridViewTextBoxColumn();
+            Column4 = new DataGridViewTextBoxColumn();
+            Column5 = new DataGridViewTextBoxColumn();
+            Column6 = new DataGridViewTextBoxColumn();
+            Column7 = new DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 12);
+            label1.Name = "label1";
+            label1.Size = new Size(77, 21);
+            label1.TabIndex = 1;
+            label1.Text = "Nombre:\r\n";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 36);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(161, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(12, 89);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(161, 23);
+            textBox2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(12, 65);
+            label2.Name = "label2";
+            label2.Size = new Size(104, 21);
+            label2.TabIndex = 3;
+            label2.Text = "Descripción:";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(12, 139);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(161, 23);
+            textBox3.TabIndex = 6;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(12, 115);
+            label3.Name = "label3";
+            label3.Size = new Size(61, 21);
+            label3.TabIndex = 5;
+            label3.Text = "Marca:";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(12, 192);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(161, 23);
+            textBox4.TabIndex = 8;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(12, 168);
+            label4.Name = "label4";
+            label4.Size = new Size(62, 21);
+            label4.TabIndex = 7;
+            label4.Text = "Precio:";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(12, 245);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(161, 23);
+            textBox5.TabIndex = 10;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(12, 221);
+            label5.Name = "label5";
+            label5.Size = new Size(57, 21);
+            label5.TabIndex = 9;
+            label5.Text = "Stock:";
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(66, 407);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(149, 23);
+            textBox6.TabIndex = 12;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(13, 407);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(47, 23);
+            numericUpDown1.TabIndex = 13;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(43, 274);
+            button1.Name = "button1";
+            button1.Size = new Size(100, 32);
+            button1.TabIndex = 14;
+            button1.Text = "Guardar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(13, 436);
+            button2.Name = "button2";
+            button2.Size = new Size(98, 32);
+            button2.TabIndex = 15;
+            button2.Text = "Nombre";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(117, 436);
+            button3.Name = "button3";
+            button3.Size = new Size(98, 32);
+            button3.TabIndex = 16;
+            button3.Text = "Cortar";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.Location = new Point(66, 383);
+            label6.Name = "label6";
+            label6.Size = new Size(90, 21);
+            label6.TabIndex = 17;
+            label6.Text = "Resultado:\r\n";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.Location = new Point(13, 383);
+            label7.Name = "label7";
+            label7.Size = new Size(31, 21);
+            label7.TabIndex = 18;
+            label7.Text = "ID:\r\n";
+            // 
+            // button4
+            // 
+            button4.Location = new Point(276, 322);
+            button4.Name = "button4";
+            button4.Size = new Size(98, 32);
+            button4.TabIndex = 20;
+            button4.Text = "Editar";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(380, 322);
+            button5.Name = "button5";
+            button5.Size = new Size(98, 32);
+            button5.TabIndex = 19;
+            button5.Text = "Borrar";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column3, Column4, Column5, Column6, Column7 });
+            dataGridView1.Location = new Point(217, 12);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(745, 304);
+            dataGridView1.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            Column1.HeaderText = "ID";
+            Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            Column2.HeaderText = "Nombre";
+            Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            Column3.HeaderText = "Descripción";
+            Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            Column4.HeaderText = "Marca";
+            Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            Column5.HeaderText = "Precio";
+            Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            Column6.HeaderText = "Stock";
+            Column6.Name = "Column6";
+            // 
+            // Column7
+            // 
+            Column7.HeaderText = "ID.Categoria";
+            Column7.Name = "Column7";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(974, 490);
+            Controls.Add(button4);
+            Controls.Add(button5);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(numericUpDown1);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(label5);
+            Controls.Add(textBox4);
+            Controls.Add(label4);
+            Controls.Add(textBox3);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(dataGridView1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label2;
+        private TextBox textBox3;
+        private Label label3;
+        private TextBox textBox4;
+        private Label label4;
+        private TextBox textBox5;
+        private Label label5;
+        private TextBox textBox6;
+        private NumericUpDown numericUpDown1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label label6;
+        private Label label7;
+        private Button button4;
+        private Button button5;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+        private DataGridViewTextBoxColumn Column3;
+        private DataGridViewTextBoxColumn Column4;
+        private DataGridViewTextBoxColumn Column5;
+        private DataGridViewTextBoxColumn Column6;
+        private DataGridViewTextBoxColumn Column7;
     }
 }
--- HerramientasCrud/Form1.cs
+++ HerramientasCrud/Form1.cs
@@ -6,5 +6,7 @@ namespace HerramientasCrud
         {
             InitializeComponent();
         }
+
+
     }
 }
